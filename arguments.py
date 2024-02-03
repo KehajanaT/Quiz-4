55b8bbb4fe6b7cb3a4d9a800a1452e65a4a0466e
Kehajana Tallie <ktallie@siue.edu>
2024-02-03 16:50:03 -0600

    Quiz4 Part 1

---
+++ arguments.py
@@ -0,0 +1,17 @@
+import argparse
+
+def store() -> None:
+    parser= argparse.ArgumentParser()
+    parser.add_argument(help="Enter the quantity",dest="quantity",type=int)
+    parser.add_argument(help="Enter an item",dest="item",type=str)    
+    parser.add_argument(help="Enter the cost",dest="price",type=float)
+
+    args=parser.parse_args()
+
+    q = args.quantity
+    i = args.item 
+    p = args.price
+
+    print("Item: ",i)
+    print("Quantity: ",q)
+    print("Price: ",p)
