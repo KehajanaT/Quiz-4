f8b47eb8a6e95cc2aeaa9e0cfc9c162a0f3b79dc
Kehajana Tallie <ktallie@siue.edu>
2024-02-05 22:19:59 -0600

    part 7

---
+++ no_property.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterable
+class Employee:
+    def __init__(self, first, last):
+        self._first = first
+        self._last = last
+        
+
+    def Name(self):
+        return self._first + " " + self._last
+
+def main():
+
+    name = Employee("Spongebob", "Squarepants")
+    print("Employee at the Krusty Krab:", name.Name())
+
+    name2 = Employee("squidward", "Tentacles")
+    print("Employee at the Krusty Krab:", name2.Name())
+
+if __name__ == "__main__":
+    main()
